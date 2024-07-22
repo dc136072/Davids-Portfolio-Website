02fa09c094dabba19fab65f5880e875581621ea0
David Cha <dc136072@gmail.com>
2024-07-21 20:55:51 -0500

    Update Project Section

---
+++ src/components/Projects/Carousel.tsx
@@ -0,0 +1,4 @@
+//                                     Carousel.tsx                                     //
+//          Credit: Large chunk of code from React Round Carousel by scriptex:          //
+//                  https://github.com/scriptex/react-round-carousel                    //
+
